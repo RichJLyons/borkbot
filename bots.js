abaa2371f2cf153a166d84cf9984c2914438d59b
RichJLyons <41702245+RichJLyons@users.noreply.github.com>
2018-11-30 19:41:38 -0600

    Add files via upload

--- bots.js
+++ bots.js
@@ -1,39 +1,24 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-           colorize: true
-           });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-                             token: auth.token,
-                             autorun: true
-                             });
-bot.on('ready', function (evt) {
-       logger.info('Connected');
-       logger.info('Logged in as: ');
-       logger.info(bot.username + ' - (' + bot.id + ')');
-       });
-bot.on('message', function (user, userID, channelID, message, evt) {
-       // Our bot needs to know if it will execute a command
-       // It will listen for messages that will start with `!`
-       if (message.substring(0, 1) == '!') {
-       var args = message.substring(1).split(' ');
-       var cmd = args[0];
-       
-       args = args.splice(1);
-       switch(cmd) {
-       // !ping
-       case 'ping':
-       bot.sendMessage({
-                       to: channelID,
-                       message: 'Pong!'
-                       });
-       break;
-       // Just add any case commands if you want to..
-       }
-       }
-       });
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity(`Hey, Wassup!`);
+});
+
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}ping`){
+    message.channel.send("Pong!");
+  }
+});
+
+bot.login(process.env.token);
