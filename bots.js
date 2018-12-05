1fcb9dc50c372a67aff065cb47ae04c2588377ff
RichJLyons <41702245+RichJLyons@users.noreply.github.com>
2018-12-05 10:06:13 -0600

    Update bots.js

--- bots.js
+++ bots.js
@@ -75,7 +75,7 @@ bot.on("message", async message => {
 //           let embed = new Discord.RichEmbed()
 //               .setImage(allowed[randomnumber].data.url);
 //           message.channel.send({embed: embed});
-          console.log(r.body[0].data);
+          console.log(r.body.data);
       });
   }
   if (cmd === `${prefix}pups`){
