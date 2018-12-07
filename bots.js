a58a8fab3ed81c82c04b2a02072ac775fd6b27dc
RichJLyons <41702245+RichJLyons@users.noreply.github.com>
2018-12-07 08:59:18 -0600

    Update bots.js

--- bots.js
+++ bots.js
@@ -82,7 +82,8 @@ bot.on("message", async message => {
       });
   }
   if (cmd === `${prefix}pups`){
-      pupsApi = pupsApi+args[0]+"/.json"
+      pupsApi = pupsApi+args[0]+"/.json";
+      message.channel.send(pupsApi).catch(console.error);
       snekfetch.get(pupsApi).then(r => {
           let rand = [];
           for (var i=0; i<r.body.data.children.length; i++){
