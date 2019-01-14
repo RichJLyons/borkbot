21568485dd44a1df3004b18e68752ebc95d65c64
RichJLyons <41702245+RichJLyons@users.noreply.github.com>
2019-01-14 12:37:38 -0600

    Update bots.js

--- bots.js
+++ bots.js
@@ -11,7 +11,12 @@ var commands = ["ping","ding","pika","bork","people","resolve","pups","free","rl
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
   bot.user.setActivity(`What is my purpose?`);
-  bot.channels.get('519900918703980552').send('Hello here!');
+  let d = new Date();
+  let n = d.getHours();
+  let nn = d.getMinutes();
+  if (n === 12 && nn === 38){
+    bot.channels.get('519900918703980552').send('Hello here!');
+  }
 });
 
 bot.on("message", async message => {
